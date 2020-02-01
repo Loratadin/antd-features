a2345ece4f4bb498cd70a260cdd2c8528ea13e01
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-02-01 10:30:26 -0600

    install ant design, add table component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,57 @@
 import React, { PureComponent } from 'react';
+import { Table } from "antd";
 
 export default class MainPage extends PureComponent {
     render() {
+        const dataSource = [
+            {
+                key: '1',
+                name: 'Mike',
+                lastName: 'Willins',
+                age: 32,
+                address: '10 Downing Street',
+                city: 'Chicago',
+
+            },
+            {
+                key: '2',
+                name: 'John',
+                lastName: 'Billards',
+                age: 42,
+                address: '5th Blvd',
+                city: 'New York',
+            },
+          ];
+
+          const columns = [
+            {
+              title: 'Name',
+              dataIndex: 'name',
+              key: 'name',
+              },
+              {
+                title: 'Last Name',
+                dataIndex: 'lastName',
+                key: 'lastName',
+              },
+            {
+              title: 'Age',
+              dataIndex: 'age',
+              key: 'age',
+            },
+            {
+              title: 'Address',
+              dataIndex: 'address',
+              key: 'address',
+              },
+              {
+                title: 'City',
+                dataIndex: 'city',
+                key: 'city',
+              },
+          ];
         return (
-            <div>Main</div>
+            <Table dataSource={dataSource} columns={columns} />
         );
     }
 }
