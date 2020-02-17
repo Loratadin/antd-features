58422dbb32bc37895d9669afc1e85e10f0b12bba
Tatsiana Shulha <taniaissohappy@gmail.com>
2020-02-17 11:40:59 -0600

    change checkbox color dynamically

--- src/components/AntdTabs.js
+++ src/components/AntdTabs.js
@@ -1,14 +1,51 @@
 import React, { PureComponent } from "react";
-import { Tabs } from "antd";
+import { Tabs, Checkbox  } from "antd";
 
 const { TabPane } = Tabs;
 export default class AntdTabs extends PureComponent {
+    getColor = color => {
+        let checkboxClassName = "checkbox-green";
+        if (color === "#FA8072") {
+            checkboxClassName = "checkbox-orange"
+        }
+        if (color === "#1890FF") {
+            checkboxClassName = "checkbox-blue"
+        }
+        return checkboxClassName;
+    };
+
     render() {
+        const dataFromAPI = [
+            {
+                key: "first",
+                name: "First",
+                color: "#008100",
+            },
+            {
+                key: "second",
+                name: "Second",
+                color: "#FA8072",
+            },
+            {
+                key: "third",
+                name: "Third",
+                color: "#1890FF",
+            },
+        ]
         return (
             <div>
                 <Tabs defaultActiveKey="1">
                     <TabPane tab="Vivamus et feugiat eros. Sed nisi quam" key="1">
-                    Content of Tab Pane 1
+                            {dataFromAPI.map(item => (
+                                <div>
+                                    <Checkbox
+                                        key={item.key}
+                                        className={this.getColor(item.color)}
+                                    >
+                                        {item.name}
+                                    </Checkbox>
+                                </div>
+                            ))}
                     </TabPane>
                     <TabPane tab="Tab 2" key="2">
                     Content of Tab Pane 2
